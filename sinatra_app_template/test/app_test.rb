88be69e5400a4d21ff26efcf756dd1ac7ca19a51
Antony Lao <antony.lao@laposte.net>
2023-03-07 14:33:19 +0000

    Update sinatra_app_template

--- sinatra_app_template/test/app_test.rb
+++ sinatra_app_template/test/app_test.rb
@@ -9,8 +9,13 @@ class AppTest < Minitest::Test
   include Rack::Test::Methods
 
   # Rack::Test::Methods require a app method that returns an instance of the Rack application
-  def app 
+  def app
     Sinatra::Application
+  end
+
+  # access the session hash
+  def session
+    last_request.env["rack.session"]
   end
 
   def test_index
